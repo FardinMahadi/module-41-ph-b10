4c2e3baa00265026be44e2d182f7911b4456d9c6
FardinMahadi <mahadihasanfardin2015@gmail.com>
2024-10-21 18:35:41 +0600

    fake data load

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Watch from "./components/Watch/Watch";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [watches, setWatches] = useState([]);
+
+  useEffect(() => {
+    fetch("watches.json")
+      .then((res) => res.json())
+      .then((data) => setWatches(data));
+  }, []);
+
+  // const watches = [
+  //   { id: 1, name: "Apple watch", price: 200 },
+  //   { id: 2, name: "MI watch", price: 200 },
+  //   { id: 3, name: "Samsu watch", price: 200 },
+  // ];
+
+  // const watches = [
+  //   { id: 1, name: "Apple Watch Series 8", price: 399 },
+  //   { id: 2, name: "Samsung Galaxy Watch 6", price: 349 },
+  //   { id: 3, name: "Garmin Venu 2", price: 299 },
+  //   { id: 4, name: "Fitbit Versa 4", price: 229 },
+  //   { id: 5, name: "Amazfit GTR 4", price: 199 },
+  // ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {watches.map((watch) => (
+        <Watch key={watch.id} watch={watch} />
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Sunglass/Sunglass.jsx
@@ -0,0 +1,26 @@
+import "./Sunglass.css";
+import Watch from "../Watch/Watch";
+// import add from "../../Utils/calculate";
+// import {
+//   add,
+//   multiply,
+//   divideTheFirstNumberByTheSecondNumber as divide,
+// } from "../../Utils/calculate";
+
+const Sunglass = () => {
+  // const first = 55;
+  // const second = 199;
+  // const sum = add(first, second);
+
+  // const mult = multiply(first, second);
+
+  // const vaag = divide(first, second);
+
+  return (
+    <div>
+      <Watch />
+    </div>
+  );
+};
+
+export default Sunglass;
